5433ffcb5b7482eac8a58b8c0e65a827fddc0939
Hodaya Mashraki <mashraki.hodaya@gmail.com>
2024-08-07 20:32:27 +0300

    change

--- part1_training.sh
+++ part1_training.sh
@@ -9,7 +9,7 @@ MAIL_USER="inbar.m@campus.technion.ac.il"
 MAIL_TYPE=ALL # Valid values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 # Define the output directory
 OUTPUT_DIR="/home/inbar.m/hw/deep_on_GPUs_HW3/out"
-NOTEBOOK_PATH="/home/inbar.m/hw/deep_on_GPUs_HW3//part1_training.sh"
+NOTEBOOK_PATH="/home/inbar.m/hw/deep_on_GPUs_HW3/Part1_Sequence.ipynb"
 
 # Create the output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
